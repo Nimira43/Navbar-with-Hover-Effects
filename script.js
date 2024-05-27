06e94df8ddd5ba48a8cda635a2a84f2fb2f62ed9
Nick Rabone <n.rabone72@aol.com>
2024-05-27 17:45:24 +0100

    Project completed.

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const menu = document.querySelector('.menu')
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('change')
 })
+
+const navLinks = [...document.querySelectorAll('.nav-link')]
+
+navLinks.forEach((link, i) => {
+    link.textContent.split('').forEach((letter) => {
+        const span = document.createElement('span')
+        span.classList.add('letter')
+        span.textContent = letter
+        link.append(span)
+    })
+    link.removeChild(link.childNodes[0])
+})
